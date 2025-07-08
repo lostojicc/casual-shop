a5f11301c1ddc59101efbd689aa541ac8efe8798
lostojicc <ostojiclukaftn@gmail.com>
2025-07-08 16:46:06 +0200

    fix: Categories endpoint await fix

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -2,7 +2,7 @@ import Category from "../models/category.model.js"
 
 export const getAllCategories = async (req, res) => {
     try {
-        const categories = Category.find();
+        const categories = await Category.find();
         res.status(200).json({
             success: true,
             categories
