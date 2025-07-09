842051ee4ffecbe3bb34e95fc331f1957e7ad434
lostojicc <ostojiclukaftn@gmail.com>
2025-07-09 04:59:25 +0200

    fix: Get category by name route fix

--- backend/src/routes/category.route.js
+++ backend/src/routes/category.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getAllCategories } from "../controllers/category.controller.js";
+import { getCategoryByName } from "../../../mobile/api/categories.js";
 
 const router = express.Router();
 
 router.get("/", getAllCategories);
-router.get("/:name");
+router.get("/:name", getCategoryByName);
 
 export default router;
