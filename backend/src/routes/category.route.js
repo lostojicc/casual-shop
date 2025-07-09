86fbcc3ca420ad799c7f532a1a3218b98b9f24d9
lostojicc <ostojiclukaftn@gmail.com>
2025-07-09 05:01:06 +0200

    fix: Get category by name from controllers

--- backend/src/routes/category.route.js
+++ backend/src/routes/category.route.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { getAllCategories } from "../controllers/category.controller.js";
-import { getCategoryByName } from "../../../mobile/api/categories.js";
+import { getAllCategories, getCategoryByName } from "../controllers/category.controller.js";
 
 const router = express.Router();
 
